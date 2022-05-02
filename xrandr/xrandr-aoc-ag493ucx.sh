2aa5f86cae008b5c67dfe5aaaf66039074d4c50f
William Smyth <codedchaos@zohomail.com>
2022-05-02 03:15:07 -0400

    [release/aoc-ag493ucx-support]
    modified variable name for display from $DISPLAY to $xDISPLAY..
    - to avoid conflict with env var
    
    Signed-off-by: William Smyth <codedchaos@zohomail.com>

--- xrandr/xrandr-aoc-ag493ucx.sh
+++ xrandr/xrandr-aoc-ag493ucx.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # 5120x1440 @ 30.00 Hz (GTF) hsync: 43.95 kHz; pclk: 293.23 MHz
 # Modeline "5120x1440_30.00"  293.23  5120 5360 5896 6672  1440 1441 1444 1465  -HSync +Vsync
 
